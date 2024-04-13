1aed830b37ccf59a2bf2e8c5dd92469415fa8f68
narimamosaddek <narima.tushty@gmail.com>
2024-04-12 22:08:39 -0400

    HWifelse16

---
+++ src/hw16IfElseCondition/Temperature.java
@@ -0,0 +1,25 @@
+package hw16IfElseCondition;
+
+import java.util.Scanner;
+
+public class Temperature {
+
+	public static void main(String[] args) {
+		System.out.println("Today's Weather");
+		Scanner scanner = new Scanner(System.in);
+		int todaysTemparature = scanner.nextInt();
+		if (todaysTemparature < 32) {
+			System.out.println("Freezing");
+		} else if (todaysTemparature < 55) {
+			System.out.println("Pleasant");
+		} else if (todaysTemparature < 73) {
+			System.out.println("Getting Warmer");
+		} else if (todaysTemparature > 101) {
+			System.out.println("Very Hot");
+		} else {
+			System.out.println("Please put a valid temperature");
+		}
+		scanner.close();
+	}
+
+}
