8c8431b674347f5422da08d0ebfff33e8c9c6071
narimamosaddek <narima.tushty@gmail.com>
2024-04-26 18:48:54 -0400

    Loop

---
+++ src/hw8UseOfLoop/Hw8UseOfLoop.java
@@ -0,0 +1,19 @@
+package hw8UseOfLoop;
+
+public class Hw8UseOfLoop {
+
+public static void main(String[] args) {
+
+		int startRange = -20;
+		int endRange = 50;
+
+		for (int i = startRange; i <= endRange; i++) {
+			if (i % 2 != 0) {
+				System.out.println("Odd Number : " + i);
+			}
+
+		}
+
+	}
+
+}
